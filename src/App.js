2629c4f0f31f4b23fad3237bf5f54d618f5de1ea
Roshan Guragain <twistfreaky@gmail.com>
2023-02-14 16:55:12 +0545

    Router-dom-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <div className="Box">
+          <Navbar/>
+          <Routes>
+            <Route path="/" exact element={<Feed/>}/>
+            <Route path="/video/:id"  element={<VideoDetail/>}/>
+            <Route path="/channel/:id"  element={<ChannelDetail/>}/>
+            <Route path="/search/:searchTerm"  element={<SearchFeed/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
